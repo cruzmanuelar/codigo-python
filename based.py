5300324cfa62c6a3955bfcd2da6acfa54d02e368
Manuel <cruzmanuelar@gmail.com>
2023-07-01 22:26:10 -0500

    Fetch all table personas

--- based.py
+++ based.py
@@ -3,8 +3,8 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user="manuel",
-    password="manuel",
+    user="root",
+    password="",
     database="basedatos"
 )
 
@@ -21,21 +21,35 @@ mycursor = mydb.cursor()
 # CONSULTA PARA ELIMINAR LA TABLA "personas"
 #mycursor.execute("DROP TABLE personas")
 
-#CONSULTA PARA CREAR LA TABLA "personas" con 6 columnas
+# CONSULTA PARA CREAR LA TABLA "personas" con 6 columnas
 #mycursor.execute("CREATE TABLE personas(id INT(10) NOT NULL AUTO_INCREMENT, nombre VARCHAR(255) NOT NULL, apellido VARCHAR(255) NOT NULL , edad INT(10) NOT NULL ,  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (id))")
 
-#CONSULTA PARA INSERTAR REGISTROS EN LA TABLA "personas"
-query   = "INSERT INTO personas (nombre, apellido, edad) VALUES (%s, %s, %s)"
-values  = [
-    ("Maxi", "Herrera", 21),
-    ("Juan", "Quevedo", 19),
-    ("Ivan", "Ruiz", 26),
-    ("Silvia", "Hernandez", 25),
-    ("German", "Aguirre", 17)
-]
-mycursor.executemany(query, values)
+# CONSULTA PARA INSERTAR REGISTROS EN LA TABLA "personas"
+#query   = "INSERT INTO personas (nombre, apellido, edad) VALUES (%s, %s, %s)"
+#values  = [
+#    ("Maxi", "Herrera", 21),
+#    ("Juan", "Quevedo", 19),
+#    ("Ivan", "Ruiz", 26),
+#    ("Silvia", "Hernandez", 25),
+#    ("German", "Aguirre", 17)
+#]
+#mycursor.executemany(query, values)
 
-mydb.commit()
+#mydb.commit()
 
-print("Se intertaron ", mycursor.rowcount, " registros")
+# ESTA CONSULTA PERMITE VISUALIZAR CUANTOS REGISTROS FUERON INSERTADOS
+#print("Se intertaron ", mycursor.rowcount, " registros")
+
+
+# ESTA CONSULTA PERMITE OBTENER TODOS LOS REGISTROS DE LA TABLA "personas"
+#mycursor.execute("SELECT * FROM personas")
+
+#data = mycursor.fetchall()
+
+# MOSTRAMOS TODA LOS REGISTROS OBTENIDOS
+#print(data)
+
+# MOSTRAR VALORES ESPECIFICAS
+#for dat in data:
+#  print(f"Nombre y apellido: {dat[1]} {dat[2]}")
 
