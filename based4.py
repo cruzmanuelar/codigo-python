df070d811aec2b697aabdebe8620b12b12117250
Manuel <cruzmanuelar@gmail.com>
2023-07-13 00:06:43 -0500

    Clear console

--- based4.py
+++ based4.py
@@ -1,4 +1,12 @@
 import mysql.connector
+import os
+
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -11,18 +19,23 @@ mycursor = mydb.cursor()
 def query_data():
 
     try:
-        print('-----Insertar nuevo registro-----')
-        name        = input('Inserte el nombre: ')
-        last_name   = input('Inserte el apellido: ')
-        age         = int(input('Inserte la edad: '))        
+        insert_row = 1
+        while insert_row == 1 or insert_row == "1":
 
-        sql = "INSERT INTO personas (nombre, apellido, edad) VALUES (%s, %s, %s)"
-        val = (name, last_name, age)
+            print('-----Insertar nuevo registro-----')
+            name        = input('Inserte el nombre: ')
+            last_name   = input('Inserte el apellido: ')
+            age         = int(input('Inserte la edad: '))        
 
-        mycursor.execute(sql, val)
-        mydb.commit()
+            sql = "INSERT INTO personas (nombre, apellido, edad) VALUES (%s, %s, %s)"
+            val = (name, last_name, age)
 
-        print(mycursor.rowcount, "record inserted.")
+            mycursor.execute(sql, val)
+            mydb.commit()
+            clear()
+            print(f"**Se ha insertado {mycursor.rowcount} registro correctamente**")
+            insert_row = input("\nInserte '1' para insertar otro registro: ")
+
     except ValueError:
         print('Ha ocurrido un error con el tipo de dato ingresado')
     except Exception as err:
